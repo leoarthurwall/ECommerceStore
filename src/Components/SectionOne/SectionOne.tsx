33dd43e0d9f560f876bf1fbc12ba74267884192f
leoarthurwall <leowall93@gmail.com>
2022-11-05 09:37:37 +0100

    created shop buttons

--- src/Components/SectionOne/SectionOne.tsx
+++ src/Components/SectionOne/SectionOne.tsx
@@ -1,34 +1,47 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const SectionOneImage = styled.div`
-height: 100vh;
-background-position: 50%;
-background-size: cover;
-position: relative;
-// background-image: url("https://static.zara.net/photos///contents/mkt/spots/aw22-north-collection/subhome-xmedia-44//w/3004/IMAGE-landscape-fill-8419161f-eed8-40f7-a5cf-cf4db412f197-default_0.jpg?ts=1667462458195");
-background-image: url("https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_1920,w_1920/enUS/Images/xcat-fw22-holiday-october-matching-sets-dual-gender-hp-large-mh-d_tcm221-958768.jpg");
+  height: 100vh;
+  background-position: 50%;
+  background-size: cover;
+  position: relative;
+  // background-image: url("https://static.zara.net/photos///contents/mkt/spots/aw22-north-collection/subhome-xmedia-44//w/3004/IMAGE-landscape-fill-8419161f-eed8-40f7-a5cf-cf4db412f197-default_0.jpg?ts=1667462458195");
+  background-image: url("https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_1920,w_1920/enUS/Images/xcat-fw22-holiday-october-matching-sets-dual-gender-hp-large-mh-d_tcm221-958768.jpg");
 
-display: flex;
-justify-content: left;
-align-items: center;
+  display: flex;
+  justify-content: left;
+  align-items: center;
 `;
 const Header = styled.h1`
-width: 30%;
-color: white;
-font-family: inter;
-font-weight: 600;
-font-size: 3.5rem;
-text-align: center;
-@media (max-width: 1024px) {
-font-size: 42px;
-}
-`
+  width: 30%;
+  color: white;
+  font-family: inter;
+  font-weight: 600;
+  font-size: 3.5rem;
+  text-align: center;
+  @media (max-width: 1024px) {
+    font-size: 42px;
+  }
+`;
+const ShopButton = styled.button`
+  background-color: white;
+  border: none;
+  color: black;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  font-family: inter;
+`;
 const SectionOne = () => {
   return (
     <SectionOneImage>
-        <Header>The ultimate experience</Header>
+      <Header>The ultimate experience</Header>
+      <ShopButton>Shop Mens</ShopButton>
+      <ShopButton>Shopw Womens</ShopButton>
     </SectionOneImage>
-  )
-}
+  );
+};
 
-export default SectionOne
+export default SectionOne;
