7eb713dd4c6697952ed7dd7132310a1157f6f3fd
leoarthurwall <leowall93@gmail.com>
2022-11-06 17:34:53 +0100

    created a use affect for the fakestore api. It consile logs the data. now I will create the UI for the results card·

--- src/Components/SearchResults/SearchResults.tsx
+++ src/Components/SearchResults/SearchResults.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+
 const SearchResultsContainer = styled.section`
   height: 100vh;
   width: 100%;
-  background-color: blue;
+  padding-top: 80px;
 `;
 
 const SearchResults = () => {
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  }, []);
+
   return <SearchResultsContainer>SearchResults</SearchResultsContainer>;
 };
 
