e279f7b4e29f7b15c6a75316f8cfd157f7e897be
leoarthurwall <leowall93@gmail.com>
2022-11-07 16:33:03 +0100

    created styled components for the image, tutle and price and added them to the item card.

--- src/Components/SearchResults/SearchResults.tsx
+++ src/Components/SearchResults/SearchResults.tsx
@@ -11,13 +11,29 @@ const SearchResultsContainer = styled.section`
 `;
 
 const ItemCard = styled.div`
-  
   width: 250px;
   height: 400px;
   background: pink;
   margin: 10px;
-  color: black;
 `;
+
+const ItemImage = styled.img`
+  width: 100%;
+  height: 70%;
+  padding: 10px;
+`;
+
+const ItemTitle = styled.h1`
+    color: black;
+    font-size: 20px;
+    font-weight: 500;
+    font-family: inter;
+`
+const ItemPrice = styled.h3`
+    font-size: 18px;
+    font-weight: 600;
+    font-family: inter;
+`
 const SearchResults = () => {
   const [data, setData] = useState<iClothes[]>([]);
   console.log({ data });
@@ -34,7 +50,11 @@ const SearchResults = () => {
   return (
     <SearchResultsContainer>
       {data.map((item, index) => (
-        <ItemCard key={index}>{item.title}</ItemCard>
+        <ItemCard key={index}>
+          <ItemImage src={item.image} alt={item.title}></ItemImage>
+          <ItemTitle>{item.title}</ItemTitle>
+          <ItemPrice>{item.price}</ItemPrice>
+        </ItemCard>
       ))}
     </SearchResultsContainer>
   );
