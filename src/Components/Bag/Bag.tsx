fc985c685b3a048bf4797a92e9c75a19013b77aa
leoarthurwall <leowall93@gmail.com>
2022-11-09 17:53:27 +0000

    added div for bagHeader

--- src/Components/Bag/Bag.tsx
+++ src/Components/Bag/Bag.tsx
@@ -7,15 +7,21 @@ const BagContainer = styled.div<IisClosed>`
     position: absolute;
     top: 80px;
     bottom:0px;
-    right: ${({ isClosed}) => isClosed ? '-300px' : '0px'};
+    right: ${({ isClosed}) => isClosed ? '-350px' : '0px'};
 
     transition: .6s;
-    width: 300px;
+    width: 350px;
     background: #1E1E1E;
     border-radius: 5px 0 0 0; /*TL TR BR BL*/
-
-
 `
+const BagHeaderBox = styled.div`
+width: 100%;
+height: 20%;
+display: flex;
+justify-content: center;
+adlgn-items: center;
+`
+
 interface IisClosed {
     isClosed: Boolean;
 }
@@ -27,7 +33,11 @@ type Props = {
 const Bag:  React.FC<Props> = (Props): ReactElement => {
     const { isClosed } = Props;
   return (
-    <BagContainer isClosed={isClosed}></BagContainer>
+    <BagContainer isClosed={isClosed}>
+        <BagHeaderBox>
+            Text
+        </BagHeadeBox>
+    </BagContainer>
   )
 }
 
