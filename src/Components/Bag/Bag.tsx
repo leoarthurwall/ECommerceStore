c3e3cf004c1ccb30dfb43e0237d801b8173ef9bd
leoarthurwall <leowall93@gmail.com>
2022-11-09 19:53:45 +0000

    added icons to the bag sidebar that will cover navbar icons

--- src/Components/Bag/Bag.tsx
+++ src/Components/Bag/Bag.tsx
@@ -1,44 +1,66 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 import { ReactElement } from "react";
-
+import { HiOutlineHeart } from "react-icons/hi";
+import { RiCloseLine } from "react-icons/ri";
+import { IconBase } from "react-icons";
 
 const BagContainer = styled.div<IisClosed>`
-    position: absolute;
-    top: 80px;
-    bottom:0px;
-    right: ${({ isClosed}) => isClosed ? '-350px' : '0px'};
+  position: absolute;
+  top: 80px;
+  bottom: 0px;
+  right: ${({ isClosed }) => (isClosed ? "-350px" : "0px")};
 
-    transition: .6s;
-    width: 350px;
-    background: #1E1E1E;
-    border-radius: 5px 0 0 0; /*TL TR BR BL*/
-`
+  transition: 0.6s;
+  width: 350px;
+  background: #1e1e1e;
+  border-radius: 5px 0 0 0; /*TL TR BR BL*/
+`;
 const BagHeaderBox = styled.div`
-width: 100%;
-height: 20%;
-display: flex;
-justify-content: center;
-adlgn-items: center;
-`
+  width: 100%;
+  height: 20%;
+  display: flex;
+  justify-content: center;
+  adlgn-items: center;
+`;
+const IconContainer = styled.div`
+  cursor: pointer;
+`;
+const IconMainContainer = styled.div`
+  height: 80px;
+  padding-right: 45px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: end;
+  gap: 1rem;
+  color: #fff;
+`;
 
 interface IisClosed {
-    isClosed: Boolean;
+  isClosed: Boolean;
 }
 
 type Props = {
-    isClosed: Boolean;
-}
+  isClosed: Boolean;
+};
 
-const Bag:  React.FC<Props> = (Props): ReactElement => {
-    const { isClosed } = Props;
+const Bag: React.FC<Props> = (Props): ReactElement => {
+  const { isClosed } = Props;
   return (
     <BagContainer isClosed={isClosed}>
-        <BagHeaderBox>
-            Text
-        </BagHeadeBox>
-    </BagContainer>
-  )
-}
+      <IconMainContainer>
+        <IconContainer>
+          <HiOutlineHeart size="24" />
+        </IconContainer>
+        <IconContainer>
+          <RiCloseLine size="30" />
+        </IconContainer>
+      </IconMainContainer>
 
-export default Bag
+      <BagHeaderBox></BagHeaderBox>
+    </BagContainer>
+  );
+};
+
+export default Bag;
