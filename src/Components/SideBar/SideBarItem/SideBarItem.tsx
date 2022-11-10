175f09c8eac55d773d5852e9a0a3673888dfa4e7
leoarthurwall <leowall93@gmail.com>
2022-11-10 15:05:00 +0000

    added styling for image and center of item card

--- src/Components/SideBar/SideBarItem/SideBarItem.tsx
+++ src/Components/SideBar/SideBarItem/SideBarItem.tsx
@@ -5,8 +5,28 @@ const ItemContainer = styled.div`
     width: 100%;
     height: 100px;
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
+`
+const ItemImage = styled.img`
+height: 100%;
+width: 20%;
+object-fit: contain;
+background: black;
+`
+const ItemCenter = styled.div`
+display: flex;
+flex-direction: column;
+`
+const ItemTitle = styled.h3`
+font-size: 20px;
+font-weight: 400px;
+color: white;
+margin: 0;
+`
+const ItemQuantity = styled.p`
+
+margin:0;
 `
 
 const SideBarItem = () => {
@@ -14,7 +34,7 @@ const SideBarItem = () => {
     <ItemContainer>
         <ItemImage></ItemImage>
         <ItemCenter>
-            <Itemtitle></Itemtitle>
+            <ItemTitle></ItemTitle>
             <ItemQuantity></ItemQuantity>
         </ItemCenter>
         <ItemRight>
