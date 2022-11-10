28ead189608bb0bcfffd466925b6669aff5b8b28
leoarthurwall <leowall93@gmail.com>
2022-11-10 21:04:23 +0000

    added checkout button

--- src/Components/SideBar/SideBarLower/SideBarlower.tsx
+++ src/Components/SideBar/SideBarLower/SideBarlower.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const SideBarlowerContainer = styled.div`
   width: 100%;
   height: 15%;
-  background: #0c0b10;
 `;
 const SubtotalContainer = styled.div`
   display: flex;
@@ -13,15 +12,26 @@ const SubtotalContainer = styled.div`
 const SubtotalText = styled.h2`
   color: grey;
   font-size: 20px;
-  font-weight: 400;
-  margin:0;
+  font-weight: 300;
+  margin: 0;
 `;
 const SubtotalAmount = styled.h2`
   color: white;
   font-size: 20px;
   font-weight: 400;
-  margin:0;
+  margin: 0;
+`;
+const CheckoutButton = styled.button`
+  width: 100%;
+  background: #0c0b10;
+  color: white;
+  outline: none;
+  border: none;
+  padding: 8px 0;
+  font-weight: 300;
+  font-size: 12px;
 
+  cursor: pointer;
 `;
 const SideBarlower = () => {
   return (
@@ -30,6 +40,7 @@ const SideBarlower = () => {
         <SubtotalText>Subtotal:</SubtotalText>
         <SubtotalAmount>$400</SubtotalAmount>
       </SubtotalContainer>
+      <CheckoutButton>CHECKOUT</CheckoutButton>
     </SideBarlowerContainer>
   );
 };
