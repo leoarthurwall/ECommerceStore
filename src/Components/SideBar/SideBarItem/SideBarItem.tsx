7935fb420521db170f3d082ccb58ec4b0b01dca4
leoarthurwall <leowall93@gmail.com>
2022-11-10 15:26:53 +0000

    finished basic styling for the item card comnponent

--- src/Components/SideBar/SideBarItem/SideBarItem.tsx
+++ src/Components/SideBar/SideBarItem/SideBarItem.tsx
@@ -1,49 +1,91 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const ItemContainer = styled.div`
-    width: 100%;
-    height: 100px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 const ItemImage = styled.img`
-height: 100%;
-width: 20%;
-object-fit: contain;
-background: black;
-`
+  height: 100%;
+  width: 20%;
+  object-fit: contain;
+  background: black;
+`;
 const ItemCenter = styled.div`
-display: flex;
-flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 const ItemTitle = styled.h3`
-font-size: 20px;
-font-weight: 400px;
-color: white;
-margin: 0;
-`
+  font-size: 20px;
+  font-weight: 400px;
+  color: white;
+  margin: 0;
+`;
 const ItemQuantity = styled.p`
+  margin: 0;
+  font-size: 16px;
+  font-weight: 16px;
+  color: grey;
+`;
 
-margin:0;
-`
+const ItemRight = styled.div`
+  width: 25px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+const ItemRemove = styled.button`
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+`;
+const ItemPrice = styled.p`
+  color: white;
+  margin: 0;
+  font-size: 18px;
+  font-weight: 400;
+`;
 
+const ItemChangeQuantityContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-itmens: center;
+  gap: 0.5rem;
+`;
+const ItemChangeQuantityButton = styled.button`
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+`;
 const SideBarItem = () => {
   return (
     <ItemContainer>
-        <ItemImage></ItemImage>
-        <ItemCenter>
-            <ItemTitle></ItemTitle>
-            <ItemQuantity></ItemQuantity>
-        </ItemCenter>
-        <ItemRight>
-            <ItemRemove></ItemRemove>
-            <ItemPrice></ItemPrice>
-            <ItemChangeQuantity></ItemChangeQuantity>
-        </ItemRight>
+      <ItemImage></ItemImage>
+      <ItemCenter>
+        <ItemTitle></ItemTitle>
+        <ItemQuantity>X</ItemQuantity>
+      </ItemCenter>
+      <ItemRight>
+        <ItemRemove></ItemRemove>
+        <ItemPrice></ItemPrice>
+        <ItemChangeQuantityContainer>
+            <ItemChangeQuantityButton>+</ItemChangeQuantityButton>
+            <ItemChangeQuantityButton>-</ItemChangeQuantityButton>
+        </ItemChangeQuantityContainer>
+      </ItemRight>
     </ItemContainer>
-  )
-}
+  );
+};
 
-export default SideBarItem
+export default SideBarItem;
