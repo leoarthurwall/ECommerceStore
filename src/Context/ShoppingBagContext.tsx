fa925fce23c02554a4fa077eda9b992ec8bfa76b
leoarthurwall <leowall93@gmail.com>
2022-11-25 18:06:16 +0100

    added the functions to the context file and then passed the function names into the values prop object so they can be used across the app

--- src/Context/ShoppingBagContext.tsx
+++ src/Context/ShoppingBagContext.tsx
@@ -1,27 +1,91 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
 type ShoppingBagProviderProps = {
-    children: ReactNode 
-}
+  children: ReactNode;
+};
+
+type BagItem = {
+  id: number;
+  quantity: number;
+};
 
 type ShoppingBagContext = {
-    getItemQuantity: (id: number) => number;
-    increaseBagQuantity: (id: number) => void;
-    decreaseBagQuantity: (id: number) => void;
-    removeFromBag: (id: number) => void;
-  }
+  getItemQuantity: (id: number) => number;
+  increaseBagQuantity: (id: number) => void;
+  decreaseBagQuantity: (id: number) => void;
+  removeFromBag: (id: number) => void;
+};
 
-const ShoppingBagContext  = createContext({})
+const ShoppingBagContext = createContext({} as ShoppingBagContext);
 
 export function useShoppingBag() {
-    return useContext(ShoppingBagContext)
+  return useContext(ShoppingBagContext);
 }
 
-
 export function ShoppingBagProvider({ children }: ShoppingBagProviderProps) {
-    return (
-        <ShoppingBagContext.Provider value={{}}>
-            {children}
-        </ShoppingBagContext.Provider>
-    )
+  const [bagItems, setBagItems] = useState<BagItem[]>([]);
+
+  // GET BAG QUANTITY
+
+  function getItemQuantity(id: number) {
+    return bagItems.find((item) => item.id === id)?.quantity || 0;
+  }
+
+  // INCREASE QUANTITY
+
+  function increaseBagQuantity(id: number) {
+    setBagItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id) == null) {
+        return [...currentItems, { id, quantity: 1 }];
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+    console.log("bag items:", bagItems);
+  }
+  // DECREASE QUANTITY
+
+  function decreaseBagQuantity(id: number) {
+    setBagItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id)?.quantity === 1) {
+        return currentItems.filter((item) => item.id !== id);
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+    console.log("quantity reduced by 1");
+  }
+
+  //REMOVE FROM BAG
+
+  function removeFromBag(id: number) {
+    setBagItems((currentItems) => {
+      return currentItems.filter((item) => item.id !== id);
+    });
+  }
+
+  return (
+    <ShoppingBagContext.Provider
+      value={{
+        getItemQuantity,
+        increaseBagQuantity,
+        decreaseBagQuantity,
+        removeFromBag,
+      }}
+    >
+      {children}
+    </ShoppingBagContext.Provider>
+  );
 }
