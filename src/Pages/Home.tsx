6d71eab51e5ff68f76670e7cd276b51e5c705e2d
leoarthurwall <leowall93@gmail.com>
2022-12-04 15:06:58 +0100

    added gap between heading and buttons on home screen - responsive on mobile landscape

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -44,7 +44,7 @@ const LeftContainer = styled.div`
     flex-direction: row;
     justify-content: space-between;
     margin-top: 5vw;
-    gap: 3rem;
+    gap: 60vh;
   }
 `;
 const Header = styled.h1`
