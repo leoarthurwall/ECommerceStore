bdffb8b0b8a970dcd5160082b41739ef34d6e2bf
leoarthurwall <leowall93@gmail.com>
2022-12-07 16:54:17 +0000

    given template for the menu component, using the sidebar for reference

---
+++ src/Components/MenuBar/MenuBar.tsx
@@ -0,0 +1,117 @@
+import React from "react";
+import styled from "styled-components";
+import { ReactElement } from "react";
+import { RiCloseLine } from "react-icons/ri";
+import { useShoppingBag } from "../../Context/ShoppingBagContext";
+
+const SideBarContainer = styled.div<IisBagClosed>`
+  position: fixed;
+  top: 0px;
+  bottom: 0px;
+  right: ${({ isBagClosed }) => (isBagClosed ? "-350px" : "0px")};
+
+  transition: 0.5s;
+  width: 350px;
+  background: #1e1e1e;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  z-index: 10;
+
+  padding: 0 25px 40px 25px;
+  box-sizing: border-box;
+
+  @media (max-width: 500px) {
+    width: 100%;
+    right: ${({ isBagClosed }) => (isBagClosed ? "-100%" : "0px")};
+  }
+`;
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.5rem;
+`;
+const HeaderText = styled.h2`
+  font-weight: 300;
+  font-size: 22px;
+`;
+const HeaderSpan = styled.span`
+  font-size: 14px;
+`;
+
+const IconContainer = styled.div`
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const SideBarUpperContainer = styled.div`
+  height: 80px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+  color: #fff;
+`;
+const SideBarMainSection = styled.div`
+  height: 65%;
+  width: 100%;
+
+  border-top: 1px solid #0c0b10;
+  border-bottom: 1px solid #0c0b10;
+
+  border-radius: 5px;
+  overflow: scroll;
+`;
+
+const SideBarOverlay = styled.div<IisBagClosed>`
+  position: fixed;
+  background-color: ${({ isBagClosed }) =>
+    isBagClosed ? "rgba(0, 0, 0, 0)" : "rgba(0, 0, 0, 0.4)"};
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 8;
+  transition: 0.5s;
+  visibility: ${({ isBagClosed }) => isBagClosed && "hidden"};
+`;
+
+interface IisBagClosed {
+  isBagClosed: Boolean;
+}
+
+const SideBar: React.FC = (): ReactElement => {
+  const {
+  
+    isBagClosed,
+    setIsBagClosed,
+    handleSidebarClick,
+  } = useShoppingBag();
+
+  // closes menu when overlay clicked
+  const handleOverlayClick = () => {
+    setIsBagClosed(!isBagClosed);
+  };
+  return (
+    <>
+      <SideBarContainer isBagClosed={isBagClosed}>
+        <SideBarUpperContainer>
+          <IconContainer onClick={(e) => handleSidebarClick(e)}>
+            <RiCloseLine size="30" />
+          </IconContainer>
+          <HeaderContainer>
+            <HeaderText>Menu</HeaderText>
+          </HeaderContainer>
+        </SideBarUpperContainer>
+        <SideBarMainSection>Men, Women, Home, Bag</SideBarMainSection>
+      </SideBarContainer>
+      <SideBarOverlay isBagClosed={isBagClosed} onClick={handleOverlayClick} />
+    </>
+  );
+};
+
+export default SideBar;
